68b8be98f2167e06082055f8843a25ece00766ba
m8rge <to.merge@gmail.com>
2014-08-21 16:40:22 +0600

    container name, upgrade

--- Ipaddr.php
+++ Ipaddr.php
@@ -9,12 +9,12 @@ namespace m8rge\OCF;
  */
 class Ipaddr extends OCF
 {
-    protected $version = '0.5';
+    protected $version = '0.9';
 
     /**
      * Container ID
      *
-     * CTID is the numeric ID of the given openvz container
+     * CTID is the numeric ID of the given openvz container. Also can be name of container
      * @var int
      */
     public $ctid;
@@ -27,6 +27,11 @@ class Ipaddr extends OCF
      */
     public $ip;
 
+    /**
+     * @var string[] Required console utilities
+     */
+    protected $requiredUtilities = ['vzlist', 'vzctl'];
+
     protected $ipv4Regex = '/^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\/(\d|[1-2]\d|3[0-2]))?$/';
     protected $ipv6Regex = '/^((?=.*::)(?!.*::.+::)(::)?([\dA-F]{1,4}:(:|\b)|){5}|([\dA-F]{1,4}:){6})((([\dA-F]{1,4}((?!\3)::|:\b|$))|(?!\2\3)){2}|(((2[0-4]|1\d|[1-9])?\d|25[0-5])\.?\b){4})$/i';
 
@@ -35,7 +40,8 @@ class Ipaddr extends OCF
         $res =  parent::validateProperties();
 
         if ($res) {
-            if (!empty($this->ctid) && !is_numeric($this->ctid)) {
+            exec('vzlist ' . escapeshellarg($this->ctid), $output, $exitCode);
+            if (!$exitCode) {
                 return false;
             }
             if (!empty($this->ip) && !preg_match($this->ipv4Regex, $this->ip) && !preg_match($this->ipv6Regex, $this->ip)) {
@@ -81,19 +87,5 @@ class Ipaddr extends OCF
         $exitCode = $this->execWithLogging($command);
 
         return $exitCode ? self::OCF_NOT_RUNNING : self::OCF_SUCCESS;
-    }
-
-    public function execWithLogging($command) //todo: extract to parent class
-    {
-        exec($command, $output, $exitCode);
-        if ($exitCode) {
-            $executable = explode(' ', $command, 2);
-            $executable = reset($executable);
-            $this->ravenClient->extra_context(['command' => $command, 'output' => $output, 'exitCode' => $exitCode]);
-            $this->ravenClient->captureException(new \Exception("$executable executed with error"));
-            return $exitCode;
-        }
-
-        return $exitCode;
     }
 }
